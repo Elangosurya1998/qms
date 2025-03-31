c93646191bca48c57ecc03f3f0e53f03e78fad52
Jenifar <166600442+jenifarjenifar@users.noreply.github.com>
2025-03-31 15:35:24 +0530

    Update Post Resource

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -6,12 +6,18 @@ use App\Filament\Resources\PostResource\Pages;
 use App\Filament\Resources\PostResource\RelationManagers;
 use App\Models\Post;
 use Filament\Forms;
+use Filament\Forms\Components\Builder\Block;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
+use Filament\Forms\Set;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
 
 class PostResource extends Resource
 {
@@ -20,31 +26,99 @@ class PostResource extends Resource
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
     protected static ?string $navigationGroup = 'CMS';
 
-    public static function form(Form $form): Form
+   public static function form(Form $form): Form
     {
         return $form
             ->schema([
                 Forms\Components\TextInput::make('title')
+                    ->unique(ignoreRecord: true)
                     ->required()
+                    ->live(onBlur: true)
+                    ->afterStateUpdated(fn (Set $set, ?string $state) => $set('slug', Str::slug($state)))
                     ->maxLength(255),
                 Forms\Components\TextInput::make('slug')
-                    ->required()
-                    ->maxLength(255),
+                    ->readOnly(),
                 Forms\Components\Textarea::make('excerpt')
                     ->columnSpanFull(),
                 Forms\Components\FileUpload::make('feature_image')
-                    ->image(),
-                Forms\Components\TextInput::make('category_id')
-                    ->maxLength(255),
+                    ->image()
+                    ->directory('file-manager/post/feature_image')
+                    ->preserveFilenames()
+                    ->maxSize(1024 * 2),
+                Forms\Components\Select::make('categories')
+                    ->relationship('categories', 'name')
+                    ->required()
+                    ->preload()
+                    ->multiple()
+                    ->columns(2),
+
                 Forms\Components\TextInput::make('author')
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('content'),
+                    ->maxLength(255)
+                    ->default(null),
+                Forms\Components\Builder::make('content')
+                    ->blocks([
+                        Block::make('heading')
+                            ->schema([
+                                TextInput::make('content')
+                                    ->label('Heading')
+                                    ->required(),
+                                Select::make('level')
+                                    ->options([
+                                        'h1' => 'Heading 1',
+                                        'h2' => 'Heading 2',
+                                        'h3' => 'Heading 3',
+                                        'h4' => 'Heading 4',
+                                        'h5' => 'Heading 5',
+                                        'h6' => 'Heading 6',
+                                    ])
+                                    ->required(),
+                            ])
+                            ->columns(2),
+                        Block::make('Rich Editor')
+                            ->schema([
+                                Forms\Components\RichEditor::make('content')
+                                    ->label('Paragraph')
+                                    ->fileAttachmentsDirectory('file-manager/post/content')
+                                    ->required(),
+                            ]),
+                        
+                        Block::make('image')
+                            ->schema([
+                                FileUpload::make('url')
+                                    ->label('Image')
+                                    ->imageEditor()
+                                    ->image()
+                                    ->openable()
+                                    ->required()
+                                    ->directory('file-manager/post/image'),
+                                TextInput::make('alt')
+                                    ->label('Alt text')
+                                    ->required(),
+                            ]),
+                        Block::make('Multiple Images')
+                            ->schema([
+                                FileUpload::make('url')
+                                    ->multiple()
+                                    ->label('Image')
+                                    ->imageEditor()
+                                    ->image()
+                                    ->openable()
+                                    ->required()
+                                    ->directory('file-manager/post/images'),
+                                TextInput::make('alt')
+                                    ->label('Alt text')
+                                    ->required(),
+                            ]),
+                    ])
+                    ->columnSpanFull(),
                 Forms\Components\Toggle::make('status')
                     ->required(),
                 Forms\Components\TextInput::make('order_by')
-                    ->numeric(),
+                    ->numeric()
+                    ->default(null),
                 Forms\Components\DatePicker::make('publish_date')
-                    ->required(),
+                    ->required()
+                    ->default(now()),
             ]);
     }
 
@@ -52,23 +126,35 @@ class PostResource extends Resource
     {
         return $table
             ->columns([
+                Tables\Columns\ImageColumn::make('feature_image'),
                 Tables\Columns\TextColumn::make('title')
+                    ->sortable()
                     ->searchable(),
                 Tables\Columns\TextColumn::make('slug')
+                    ->sortable()
                     ->searchable(),
-                Tables\Columns\ImageColumn::make('feature_image'),
-                Tables\Columns\TextColumn::make('category_id')
+                Tables\Columns\TextColumn::make('slug_url')
+                    ->icon('heroicon-o-clipboard')
+                    ->tooltip('Copy Link')
+                    ->limit(20)
+                    ->label('Link')
+                    ->copyable()
+                    ->copyMessage('Link copied'),
+                Tables\Columns\TextColumn::make('category_names')
+                    ->sortable()
                     ->searchable(),
                 Tables\Columns\TextColumn::make('author')
-                    ->searchable(),
+                    ->sortable()
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\IconColumn::make('status')
                     ->boolean(),
-                Tables\Columns\TextColumn::make('order_by')
-                    ->numeric()
-                    ->sortable(),
+
                 Tables\Columns\TextColumn::make('publish_date')
                     ->date()
-                    ->sortable(),
+                    ->sortable()
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
@@ -81,8 +167,13 @@ class PostResource extends Resource
             ->filters([
                 //
             ])
-            ->actions([
-                Tables\Actions\EditAction::make(),
+              ->actions([
+                Tables\Actions\ActionGroup::make([
+                    Tables\Actions\ViewAction::make(),
+                    Tables\Actions\EditAction::make(),
+                    Tables\Actions\DeleteAction::make(),
+                ])
+                
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
