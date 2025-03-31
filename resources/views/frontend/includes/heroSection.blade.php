95243b1771235d0ad16ad237abc9301dc066311d
Jenifar <166600442+jenifarjenifar@users.noreply.github.com>
2025-03-31 17:47:49 +0530

    Changes Blade File

---
+++ resources/views/frontend/includes/heroSection.blade.php
@@ -0,0 +1,41 @@
+<div id="wRSD92DWWGR1YRP6" class="mwPageBlock Include" style="">
+    <div class="blockContents">
+
+        <div class="videoBanner videoBannerOne videoBannerLarge content-style">
+            <div class="videoBannerWrap"
+                style="background-image: url('{{ asset('frontend/files/galleries/BFC_Video_Placeholder.jpg') }}')"
+                data-fit-img>
+                <video autoplay="autoplay" loop="loop" muted="muted"
+                    poster="{{ asset('frontend/files/galleries/BFC_Video_Placeholder.jpg') }}" data-fit-img-child>
+                    <source src="{{ asset('frontend/files/galleries/Sequence_01.webm') }}" type="video/mp4" />
+                    <img class="_img-fluid" alt=""
+                        src="{{ asset('frontend/files/galleries/BFC_Video_Placeholder.jpg') }}" />
+                </video>
+                <div class="videoBannerInner">
+                    <div class="container">
+                        <h1 class="videoBannerTitle">
+
+                            Ready to start your next
+                        </h1>
+                        <p class="videoBannerParagraph">Adventure?</p>
+
+                        <div class="decoration">
+                            <div class="decoLine _mx-auto _mb-30 _bg-secondary" style="width: 2px; height: 128.5px;">
+                            </div>
+                        </div>
+
+                        <div class="videoBannerBtns">
+
+
+
+                        </div>
+                    </div>
+                </div>
+                <div class="playbutton"><a href="%7bvideoBanner_fulllength%7d.html" class="video-popup"><img
+                            src="{{ asset('frontend/images/play-button.png') }}"> Play Full Video</a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
