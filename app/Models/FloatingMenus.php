56d7a6e36acc4718a948510575e9a330d1d425b8
Jenifar <166600442+jenifarjenifar@users.noreply.github.com>
2025-03-31 14:29:00 +0530

    Create Migration Files

---
+++ app/Models/FloatingMenus.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FloatingMenus extends Model
+{
+    /** @use HasFactory<\Database\Factories\FloatingMenusFactory> */
+    use HasFactory;
+}
---
+++ app/Models/PivotCategory.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PivotCategory extends Model
+{
+    /** @use HasFactory<\Database\Factories\PivotCategoryFactory> */
+    use HasFactory;
+}
