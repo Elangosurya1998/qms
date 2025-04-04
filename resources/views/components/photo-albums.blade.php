a4dd9fe6ec2c18afdd68ce6ea4981edb0be94c05
Jenifar <108123980+Jenifarjeni@users.noreply.github.com>
2025-04-04 21:55:04 +0530

    photo gallery

---
+++ resources/views/components/photo-albums.blade.php
@@ -0,0 +1,219 @@
+ @props([
+     'tittle' => null,
+     'description' => null,
+     'contactPerson' => null,
+     'randomImages' => null,
+     'galleryTitle' => null,
+     'link' => null,
+ ])
+ <div id="contentArea-HY18X2" class="contentArea contentAreaLarge _bg-white content-style">
+     <div class="contentAreaWrap">
+         <div class="container">
+             <div class="mwPageArea">
+                 <div id="wX7GXXDG3AYVY85B" class="mwPageBlock Content" style="">
+                     <div class="blockContents">
+                         <h2>
+                             {{ $tittle }}
+                         </h2>
+                     </div>
+                 </div>
+                 <div id="wGORRG1ZRRYURGEQ" class="mwPageBlock Content" style="">
+                     <div class="blockContents">
+                         <p> {{ $description }}<a rel="noopener"> {{ $contactPerson }} </a>.
+                         </p>
+                     </div>
+                 </div>
+
+                 <div id="w6ARXPSMFOZL85H7" class="mwPageBlock Spacer" style="">
+                     <div class="blockContents">
+                         <div class="mwSpacer hor" style="height:15px;"></div>
+                     </div>
+                 </div>
+
+                 <div id="wYEUQWEKUC92RA02" class="mwPageBlock Include" style="">
+                     <div class="blockContents">
+                         <div class="fourCol row">
+                             <div class="fourColItem col-lg-3 col-sm-6">
+                                 <div class="mwPageArea">
+                                     <div id="wQLJFJE0Q6X95BV7" class="mwPageBlock File" style="">
+                                         <div class="blockContents">
+                                             <div class="mwFileEmbed Image" style="overflow:hidden">
+
+                                                 <a href="Brain-Bowl-2025.html" target=""
+                                                     style="display: block !important; ">
+
+                                                     <img id="image_IA3MNEK30LJ5AQ05" src="{{ $randomImages }}"
+                                                         class="mwFile Image full imgEd"
+                                                         style="display: block; border:none;width:100%; margin: 0 0%;"
+                                                         alt="BrainBowl25_Morweb.jpg" />
+
+                                                 </a>
+
+                                             </div>
+                                             <script type="text/javascript">
+                                                 (function() {
+                                                     // Small timeout for browser to catch up
+                                                     //	setTimeout(function() {
+
+                                                     // Getting image to setup				
+                                                     var $img = document.getElementById('image_IA3MNEK30LJ5AQ05');
+                                                     mwLoadImage($img, 300, 300);
+
+                                                     //	}, 1);
+                                                 })();
+                                             </script>
+                                         </div>
+                                     </div>
+                                     <div id="wZ9V4ZFL8V0N1OIA" class="mwPageBlock Content" style="">
+                                         <div class="blockContents  pad-top-s">
+                                             <h5 style="text-align: center;">
+                                                 <a href="{{ $link }}">{{ $galleryTitle }}</a>
+                                             </h5>
+                                         </div>
+                                     </div>
+                                     <div class="Clear"></div>
+                                 </div>
+                             </div>
+                             <div class="fourColItem col-lg-3 col-sm-6">
+                                 <div class="mwPageArea">
+                                     <div id="wK52POTY1F1VOROQ" class="mwPageBlock File" style="">
+                                         <div class="blockContents">
+                                             <div class="mwFileEmbed Image" style="overflow:hidden">
+
+                                                 <a href="She-Stoops-to-Conquer.html" target=""
+                                                     style="display: block !important; ">
+
+                                                     <img id="image_II6CUPP0S7QNZYBQ"
+                                                         src="files/_cache/966c14bc681ddd50f56390d05565e23b.jpg"
+                                                         class="mwFile Image full imgEd"
+                                                         style="display: block; border:none;width:100%; margin: 0 0%;"
+                                                         alt="SSTC_Performance-27-0001.jpg" />
+
+                                                 </a>
+
+                                             </div>
+                                             <script type="text/javascript">
+                                                 (function() {
+                                                     // Small timeout for browser to catch up
+                                                     //	setTimeout(function() {
+
+                                                     // Getting image to setup				
+                                                     var $img = document.getElementById('image_II6CUPP0S7QNZYBQ');
+                                                     mwLoadImage($img, 300, 300);
+
+                                                     //	}, 1);
+                                                 })();
+                                             </script>
+                                         </div>
+                                     </div>
+                                     <div id="wNMKOXMTAVWAP0D8" class="mwPageBlock Content" style="">
+                                         <div class="blockContents  pad-top-s">
+                                             <h5 style="text-align: center;">
+                                                 "<a href="She-Stoops-to-Conquer.html">She Stoops to Conquer</a>"
+                                             </h5>
+                                         </div>
+                                     </div>
+                                     <div class="Clear"></div>
+                                 </div>
+                             </div>
+                             <div class="fourColItem col-lg-3 col-sm-6">
+                                 <div class="mwPageArea">
+                                     <div id="w2ICOOAG18VAF0O4" class="mwPageBlock File" style="">
+                                         <div class="blockContents">
+                                             <div class="mwFileEmbed Image" style="overflow:hidden">
+
+                                                 <a href="2024-Venture-Royale-Annual-Auction.html" target=""
+                                                     style="display: block !important; ">
+
+                                                     <img id="image_IVKB0IXJEMOPJB85"
+                                                         src="files/_cache/f8e8f2645c44308c8638c53d77f5ec1e.jpg"
+                                                         class="mwFile Image full imgEd"
+                                                         style="display: block; border:none;width:100%; margin: 0 0%;"
+                                                         alt="Auction2024-49-0001.jpg" />
+
+                                                 </a>
+
+                                             </div>
+                                             <script type="text/javascript">
+                                                 (function() {
+                                                     // Small timeout for browser to catch up
+                                                     //	setTimeout(function() {
+
+                                                     // Getting image to setup				
+                                                     var $img = document.getElementById('image_IVKB0IXJEMOPJB85');
+                                                     mwLoadImage($img, 300, 300);
+
+                                                     //	}, 1);
+                                                 })();
+                                             </script>
+                                         </div>
+                                     </div>
+                                     <div id="wXVWIM4VFQGJ1G6F" class="mwPageBlock Content" style="">
+                                         <div class="blockContents  pad-top-s">
+                                             <h5 style="text-align: center;">
+                                                 <a href="2024-Venture-Royale-Annual-Auction.html">2024 Annual Auction
+                                                     &mdash;
+                                                     Venture Royale</a>
+                                             </h5>
+                                         </div>
+                                     </div>
+                                     <div class="Clear"></div>
+                                 </div>
+                             </div>
+                             <div class="fourColItem col-lg-3 col-sm-6">
+                                 <div class="mwPageArea">
+                                     <div id="w3QII5BDSKOSVK6E" class="mwPageBlock File" style="">
+                                         <div class="blockContents">
+                                             <div class="mwFileEmbed Image" style="overflow:hidden">
+
+                                                 <a href="2024-Christmas-Concert.html" target=""
+                                                     style="display: block !important; ">
+
+                                                     <img id="image_INJR2A9TY5PFYOKP"
+                                                         src="files/_cache/0cf378ee4d6004c953d45d9e421021fb.jpg"
+                                                         class="mwFile Image full imgEd"
+                                                         style="display: block; border:none;width:100%; margin: 0 0%;"
+                                                         alt="2024XmasConcert_Rehearsals-33-0001.jpg" />
+
+                                                 </a>
+
+                                             </div>
+                                             <script type="text/javascript">
+                                                 (function() {
+                                                     // Small timeout for browser to catch up
+                                                     //	setTimeout(function() {
+
+                                                     // Getting image to setup				
+                                                     var $img = document.getElementById('image_INJR2A9TY5PFYOKP');
+                                                     mwLoadImage($img, 300, 300);
+
+                                                     //	}, 1);
+                                                 })();
+                                             </script>
+                                         </div>
+                                     </div>
+                                     <div id="w81XQEG6PL3HHS27" class="mwPageBlock Content" style="">
+                                         <div class="blockContents  pad-top-s">
+                                             <h5 style="text-align: center;">
+                                                 <a href="2024-Christmas-Concert.html">"Christmas in Michigan"
+                                                     Concert</a>
+                                             </h5>
+                                         </div>
+                                     </div>
+                                     <div class="Clear"></div>
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+                 <div id="w2HN2NMT5TDUDZYD" class="mwPageBlock Spacer" style="">
+                     <div class="blockContents">
+                         <div class="mwSpacer hor" style="height:15px;"></div>
+                     </div>
+                 </div>
+
+                 <div class="Clear"></div>
+             </div>
+         </div>
+     </div>
+ </div>
